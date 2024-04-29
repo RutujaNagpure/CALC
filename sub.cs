0aa6c1ef2e0b66ceaadfaa441b2d1d9aee95d783
RutujaNagpure <126090559+RutujaNagpure@users.noreply.github.com>
2024-04-30 01:33:11 +0530

    Create sub.cs

---
+++ sub.cs
@@ -0,0 +1,23 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Input variables
+        int num1, num2;
+
+        // Getting input from user
+        Console.WriteLine("Enter the first number:");
+        num1 = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Enter the second number:");
+        num2 = Convert.ToInt32(Console.ReadLine());
+
+        // Subtracting two numbers
+        int difference = num1 - num2;
+
+        // Displaying the result
+        Console.WriteLine($"The difference of {num1} and {num2} is {difference}");
+    }
+}
